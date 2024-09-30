eaad88819c25f56a96a37cfdc2b4cda69cb111d3
huseyingedek <hgedek1881@gmail.com>
2024-09-30 23:50:19 +0300

    products

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import Header from "./components/header/Header";
 import Categories from "./components/categories/Categories";
+import Products from "./components/products/Products";
+import CartTotals from "./components/cart/CartTotals";
 
 function App() {
   return (
     <>
       <Header />
       <div className="home px-6 flex justify-between gap-10">
-        <div className="categories flex-1 overflow-auto max-h-[calc(100vh-_-112px)] pb-64">
+        <div className="categories overflow-auto max-h-[calc(100vh-_-112px)] pb-64">
           <Categories />
         </div>
         <div className="products flex-[8]">
-          <div>products</div>
+          <div>
+            <Products />
+          </div>
         </div>
-        <div className="">
-          <div>cart totals</div>
+        <div className="cart-wrapper min-w-[300px] md:mt-[24px] border">
+          <CartTotals />
         </div>
       </div>
     </>
---
+++ src/components/products/Products.jsx
@@ -0,0 +1,138 @@
+const Products = () => {
+  return (
+    <div className="products-wrapper grid grid-cols-card gap-4">
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+        <div className="products-img">
+          <img
+            src="https://i.lezzet.com.tr/images-xxlarge-secondary/elmalari-degerlendirmenin-x-yolu-71ee2e39-809d-45ff-a97c-d21d231931f9.jpg"
+            alt=""
+            className="h-28 object-cover w-full border-b"
+          />
+        </div>
+        <div className="product-info flex flex-col p-3">
+          <span className="font-bold">Elma</span>
+          <span>12₺</span>
+        </div>
+      </div>
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+        <div className="products-img">
+          <img
+            src="https://i.lezzet.com.tr/images-xxlarge-secondary/elmalari-degerlendirmenin-x-yolu-71ee2e39-809d-45ff-a97c-d21d231931f9.jpg"
+            alt=""
+            className="h-28 object-cover w-full border-b"
+          />
+        </div>
+        <div className="product-info flex flex-col p-3">
+          <span className="font-bold">Elma</span>
+          <span>12₺</span>
+        </div>
+      </div>
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+        <div className="products-img">
+          <img
+            src="https://i.lezzet.com.tr/images-xxlarge-secondary/elmalari-degerlendirmenin-x-yolu-71ee2e39-809d-45ff-a97c-d21d231931f9.jpg"
+            alt=""
+            className="h-28 object-cover w-full border-b"
+          />
+        </div>
+        <div className="product-info flex flex-col p-3">
+          <span className="font-bold">Elma</span>
+          <span>12₺</span>
+        </div>
+      </div>
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+        <div className="products-img">
+          <img
+            src="https://i.lezzet.com.tr/images-xxlarge-secondary/elmalari-degerlendirmenin-x-yolu-71ee2e39-809d-45ff-a97c-d21d231931f9.jpg"
+            alt=""
+            className="h-28 object-cover w-full border-b"
+          />
+        </div>
+        <div className="product-info flex flex-col p-3">
+          <span className="font-bold">Elma</span>
+          <span>12₺</span>
+        </div>
+      </div>
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+        <div className="products-img">
+          <img
+            src="https://i.lezzet.com.tr/images-xxlarge-secondary/elmalari-degerlendirmenin-x-yolu-71ee2e39-809d-45ff-a97c-d21d231931f9.jpg"
+            alt=""
+            className="h-28 object-cover w-full border-b"
+          />
+        </div>
+        <div className="product-info flex flex-col p-3">
+          <span className="font-bold">Elma</span>
+          <span>12₺</span>
+        </div>
+      </div>
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+        <div className="products-img">
+          <img
+            src="https://i.lezzet.com.tr/images-xxlarge-secondary/elmalari-degerlendirmenin-x-yolu-71ee2e39-809d-45ff-a97c-d21d231931f9.jpg"
+            alt=""
+            className="h-28 object-cover w-full border-b"
+          />
+        </div>
+        <div className="product-info flex flex-col p-3">
+          <span className="font-bold">Elma</span>
+          <span>12₺</span>
+        </div>
+      </div>
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+        <div className="products-img">
+          <img
+            src="https://i.lezzet.com.tr/images-xxlarge-secondary/elmalari-degerlendirmenin-x-yolu-71ee2e39-809d-45ff-a97c-d21d231931f9.jpg"
+            alt=""
+            className="h-28 object-cover w-full border-b"
+          />
+        </div>
+        <div className="product-info flex flex-col p-3">
+          <span className="font-bold">Elma</span>
+          <span>12₺</span>
+        </div>
+      </div>
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+        <div className="products-img">
+          <img
+            src="https://i.lezzet.com.tr/images-xxlarge-secondary/elmalari-degerlendirmenin-x-yolu-71ee2e39-809d-45ff-a97c-d21d231931f9.jpg"
+            alt=""
+            className="h-28 object-cover w-full border-b"
+          />
+        </div>
+        <div className="product-info flex flex-col p-3">
+          <span className="font-bold">Elma</span>
+          <span>12₺</span>
+        </div>
+      </div>
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+        <div className="products-img">
+          <img
+            src="https://i.lezzet.com.tr/images-xxlarge-secondary/elmalari-degerlendirmenin-x-yolu-71ee2e39-809d-45ff-a97c-d21d231931f9.jpg"
+            alt=""
+            className="h-28 object-cover w-full border-b"
+          />
+        </div>
+        <div className="product-info flex flex-col p-3">
+          <span className="font-bold">Elma</span>
+          <span>12₺</span>
+        </div>
+      </div>
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+        <div className="products-img">
+          <img
+            src="https://i.lezzet.com.tr/images-xxlarge-secondary/elmalari-degerlendirmenin-x-yolu-71ee2e39-809d-45ff-a97c-d21d231931f9.jpg"
+            alt=""
+            className="h-28 object-cover w-full border-b"
+          />
+        </div>
+        <div className="product-info flex flex-col p-3">
+          <span className="font-bold">Elma</span>
+          <span>12₺</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Products;
