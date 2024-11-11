1decff40bff2413ebaec0df1eb48248b6726df45
Hüseyin <hgedek1881@gmail.com>
2024-11-11 23:15:25 +0300

    asdads

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -105,32 +105,6 @@ const Products = () => {
           <span>12₺</span>
         </div>
       </div>
-      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
-        <div className="products-img">
-          <img
-            src="https://i.lezzet.com.tr/images-xxlarge-secondary/elmalari-degerlendirmenin-x-yolu-71ee2e39-809d-45ff-a97c-d21d231931f9.jpg"
-            alt=""
-            className="h-28 object-cover w-full border-b"
-          />
-        </div>
-        <div className="product-info flex flex-col p-3">
-          <span className="font-bold">Elma</span>
-          <span>12₺</span>
-        </div>
-      </div>
-      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
-        <div className="products-img">
-          <img
-            src="https://i.lezzet.com.tr/images-xxlarge-secondary/elmalari-degerlendirmenin-x-yolu-71ee2e39-809d-45ff-a97c-d21d231931f9.jpg"
-            alt=""
-            className="h-28 object-cover w-full border-b"
-          />
-        </div>
-        <div className="product-info flex flex-col p-3">
-          <span className="font-bold">Elma</span>
-          <span>12₺</span>
-        </div>
-      </div>
     </div>
   );
 };
