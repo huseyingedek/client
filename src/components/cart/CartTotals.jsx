7fbc7316a3d7ec923b1436196335d76dd21df618
Hüseyin <hgedek1881@gmail.com>
2024-11-12 13:48:40 +0300

    add cart

--- src/components/cart/CartTotals.jsx
+++ src/components/cart/CartTotals.jsx
@@ -1,14 +1,41 @@
 import { Button } from "antd";
-import { ClearOutlined } from '@ant-design/icons';
+import { ClearOutlined, PlusCircleOutlined, MinusCircleOutlined } from '@ant-design/icons';
+
 const CartTotals = () => {
   return (
     <div className="cart h-full max-h-[calc(100vh_-_90px)] flex flex-col">
       <h2 className="bg-blue-600 text-center py-4 text-white font-bold tracking-wide">
         Sepetteki Ürünler
       </h2>
-      <div className="cart-items">
-        <div className="cart-item">cart item</div>
-      </div>
+      <ul className="cart-items px-2 flex flex-col gap-y-3 py-2 overflow-y-auto">
+        <li className="cart-item flex justify-between">
+          <div className="flex items-center">
+            <img src="https://i.lezzet.com.tr/images-xxlarge-secondary/elmalari-degerlendirmenin-x-yolu-71ee2e39-809d-45ff-a97c-d21d231931f9.jpg"
+              alt=""
+              className="w-16 h-16 object-cover"
+            />
+            <div className="flex flex-col ml-2">
+              <b>Elma</b>
+              <span>12₺ x 2</span>
+            </div>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <Button
+              type="primary"
+              size="small"
+              className="w-full flex items-center justify-center rounded-full"
+              icon={<PlusCircleOutlined />}
+            />
+            <span>1</span>
+            <Button
+              type="primary"
+              size="small"
+              className="w-full flex items-center justify-center rounded-full"
+              icon={<MinusCircleOutlined />}
+            />
+          </div>
+        </li>
+      </ul>
       <div className="cart-totals mt-auto">
         <div className="border-t border-b">
           <div className="flex justify-between p-2">
@@ -33,7 +60,7 @@ const CartTotals = () => {
           <Button
             type="primary"
             size="large"
-            className="w-full mt-2 flex items-center justify-center"
+            className="w-full flex items-center justify-center"
             icon={<ClearOutlined />}
             danger
           >
--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -110,3 +110,4 @@ const Products = () => {
 };
 
 export default Products;
+  
