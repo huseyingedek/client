8fc87a88a2851cef71d74dbb4363b97255b0f1af
Hüseyin <hgedek1881@gmail.com>
2024-11-14 23:08:53 +0300

    asdads

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,40 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import Header from "./components/header/Header";
 import HomePage from "./pages/HomePage";
 import CartPage from "./pages/CartPage";
 import BillPage from "./pages/BillPage";
 import Customers from "./pages/CustomerPage";
-import StatisticPage from "./pages/StatisticPage"
+import StatisticPage from "./pages/StatisticPage";
+import Register from "./pages/Auth/Register";
+import Login from "./pages/Auth/Login";
+
 function App() {
   return (
     <BrowserRouter>
-      <Header />
+      <AppRoutes />
+    </BrowserRouter>
+  );
+}
+
+function AppRoutes() {
+  const location = useLocation();
+  const currentRoute = location.pathname.split("/")[1];
+  const isAuthPage = currentRoute === "login" || currentRoute === "register";
+
+  return (
+    <>
+      {!isAuthPage && <Header />}
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/cart" element={<CartPage />} />
         <Route path="/bills" element={<BillPage />} />
         <Route path="/customers" element={<Customers />} />
         <Route path="/statistic" element={<StatisticPage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
 
--- src/pages/StatisticPage.jsx
+++ src/pages/StatisticPage.jsx
@@ -1,10 +1,61 @@
 import React from 'react'
 import StatisticCard from '../components/statistics/StatisticCard'
+import { Area } from '@ant-design/plots';
+import { Pie } from '@ant-design/plots';
 
 const StatisticPage = () => {
+    const config = {
+        data: {
+            type: 'fetch',
+            value: 'https://assets.antv.antgroup.com/g2/aapl.json',
+        },
+        xField: (d) => new Date(d.date),
+        yField: 'close',
+    };
+
+    const Pieconfig = {
+        data: [
+            { type: '分类一', value: 27 },
+            { type: '分类二', value: 25 },
+            { type: '分类三', value: 18 },
+            { type: '分类四', value: 15 },
+            { type: '分类五', value: 10 },
+            { type: '其他', value: 5 },
+        ],
+        angleField: 'value',
+        colorField: 'type',
+        innerRadius: 0.6,
+        label: {
+            text: 'value',
+            style: {
+                fontWeight: 'bold',
+            },
+        },
+        legend: {
+            color: {
+                title: false,
+                position: 'right',
+                rowPadding: 5,
+            },
+        },
+        annotations: [
+            {
+                type: 'text',
+                style: {
+                    text: 'AntV\nCharts',
+                    x: '50%',
+                    y: '50%',
+                    textAlign: 'center',
+                    fontSize: 40,
+                    fontStyle: 'bold',
+                },
+            },
+        ],
+    };
+
     return (
         <>
-            <div className='px-6'>
+            <div className='px-6 md:pb-0 pb-20'>
                 <h1 className='text-4xl font-bold text-center mb-4'>İstatistiklerim</h1>
                 <div className='statistic-section'>
                     <h2 className='text-lg'>Hoş Geldin{" "} <span className='text-green-700 text-bold text-xl'>Admin</span></h2>
@@ -13,7 +64,14 @@ const StatisticPage = () => {
                         <StatisticCard title={"Toplam Kazanç"} amout={"660.96 ₺"} img={"images/money.png"} />
                         <StatisticCard title={"Toplam Satış"} amout={"6"} img={"images/sale.png"} />
                         <StatisticCard title={"Toplam Ürün"} amout={"28"} img={"images/product.png"} />
-
+                    </div>
+                    <div className='flex justify-between gap-10 lg:flex-row flex-col items-center'>
+                        <div className='lg:w-1/2 lg:h-full h-72 w-[100%]'>
+                            <Area {...config} />
+                        </div>
+                        <div className='lg:w-1/2 lg:h-full h-full w-[100%]'>
+                            <Pie {...Pieconfig} />
+                        </div>
                     </div>
                 </div>
             </div>
