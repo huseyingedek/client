241313ce44961c1d21b8d2b221df9212eb118a7b
Hüseyin <hgedek1881@gmail.com>
2024-11-21 20:17:30 +0300

    add categories fetch post

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -1,43 +1,75 @@
+import { PlusOutlined } from "@ant-design/icons"
+import "./style.css"
+import { useState } from "react";
+import { Form, Input, Button, Modal, message } from "antd";
+
 const Categories = () => {
+  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+  const [form]= Form.useForm()
+  const onFinish = (values) => {
+    try {
+      fetch("http://localhost:5000/api/categories/add-category", {
+        method: "POST",
+        body: JSON.stringify(values),
+        headers: { "Content-type": "application/json; charset=UTF-8" },
+      });
+      message.success("Kategori Başarıyla Eklendi");
+      form.resetFields();
+      setIsAddModalOpen(false);
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
     <ul className="flex gap-6 md:flex-col text-lg">
-      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]">
+      <li className="category-item">
         <span>Tümü</span>
       </li>
-      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]">
+      <li className="category-item">
         <span>Yiyecek</span>
       </li>
-      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]">
+      <li className="category-item">
         <span>İçecek</span>
       </li>
-      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]">
+      <li className="category-item">
         <span>İçecek</span>
       </li>
-      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]">
+      <li className="category-item">
         <span>İçecek</span>
       </li>
-      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]">
+      <li className="category-item">
         <span>İçecek</span>
       </li>
-      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]">
+      <li className="category-item">
         <span>İçecek</span>
       </li>
-      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]">
+      <li className="category-item">
         <span>İçecek</span>
       </li>
-      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]">
+      <li className="category-item">
         <span>İçecek</span>
       </li>
-      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]">
+      <li className="category-item">
         <span>İçecek</span>
       </li>
-      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]">
+      <li className="category-item">
         <span>İçecek</span>
       </li>
-      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]">
-        <span>İçecek</span>
-      </li> 
-      
+      <li className="category-item !bg-purple-800 hover:opacity-90" onClick={() => setIsAddModalOpen(true)}>
+        <PlusOutlined className="md:text-2xl" />
+      </li>
+      <Modal title="Yeni Kategori Ekle" open={isAddModalOpen} onCancel={() => setIsAddModalOpen(false)} footer={false}>
+        <Form layout="vertical" onFinish={onFinish} form={form}>
+          <Form.Item name={"title"} label="Kategori Ekle" rules={[{ required: true, message: "Kategori Alanı Boş Geçilemez" }]}>
+            <Input />
+          </Form.Item>
+          <Form.Item className="flex justify-end mb-0">
+            <Button type="primary" htmlType="submit">
+              Oluştur
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
     </ul>
   );
 };
