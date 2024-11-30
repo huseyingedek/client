05d11d48e0d1b61ef20f35e842568e0b0c8a0671
Hüseyin <hgedek1881@gmail.com>
2024-11-30 20:19:26 +0300

    get-all products

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,35 +1,40 @@
+import { useEffect, useState } from "react";
+
 const Products = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await fetch("http://localhost:5000/api/products/get-all");
+        const data = await res.json();
+        setProducts(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getProducts();
+  }, []);
+
   return (
     <div className="products-wrapper grid grid-cols-card gap-4">
-      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
-        <div className="products-img">
-          <img
-            src="https://i.lezzet.com.tr/images-xxlarge-secondary/elmalari-degerlendirmenin-x-yolu-71ee2e39-809d-45ff-a97c-d21d231931f9.jpg"
-            alt=""
-            className="h-28 object-cover w-full border-b"
-          />
+      {products.map((item) => (
+        <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+          <div className="products-img">
+            <img
+              src={item.image}
+              alt=""
+              className="h-28 object-cover w-full border-b"
+            />
+          </div>
+          <div className="product-info flex flex-col p-3">
+            <span className="font-bold">{item.title}</span>
+            <span>{item.price}₺</span>
+          </div>
         </div>
-        <div className="product-info flex flex-col p-3">
-          <span className="font-bold">Elma</span>
-          <span>12₺</span>
-        </div>
-      </div>
-      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
-        <div className="products-img">
-          <img
-            src="https://i.lezzet.com.tr/images-xxlarge-secondary/elmalari-degerlendirmenin-x-yolu-71ee2e39-809d-45ff-a97c-d21d231931f9.jpg"
-            alt=""
-            className="h-28 object-cover w-full border-b"
-          />
-        </div>
-        <div className="product-info flex flex-col p-3">
-          <span className="font-bold">Elma</span>
-          <span>12₺</span>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
 
 export default Products;
-  
